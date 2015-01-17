1ebff561eb54600569db0d52f358d5f0cc9d61e7
Odd Andreas Sørsæther <osorseth@gmail.com>
2015-01-17 15:31:16 +0100

    Works fairly well at this point. Missing certain UI finesses like error handling (being disconnected while ripping is disastrous to the tagging process) and rescanning of the bluray disc.
    Signed-off-by: Odd Andreas Sørsæther <osorseth@gmail.com>

--- GUI/main.py
+++ GUI/main.py
@@ -8,15 +8,16 @@ import threading
 import handbrake
 from GUI import RipperWindow
 from OddTools.GUI import fileselect
+import subprocess
 
 
 class MainWindow(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
+        self.master = master
         self.config()
         self.pack()
-        self.path = tk.StringVar(value="" if oddconfig.get_setting('drive') is None
-                else oddconfig.get_setting('drive'))
+        self.path = tk.StringVar(value="")
         in_path = fileselect.fileselect(self, self.path, True)
         in_path.pack()
         self.starter = tk.Button(self, text="Start rip",
@@ -25,24 +26,24 @@ class MainWindow(tk.Frame):
 
     def start_rip(self):
         self.starter.config(state=tk.DISABLED)
-        st = ScanThread(self.path.get(), self.starter)
+        st = ScanThread(self.path.get(), self.starter, self.master)
         st.start()
 
 
 class ScanThread(threading.Thread):
-    def __init__(self, path, button):
+    def __init__(self, path, button, master):
         threading.Thread.__init__(self)
         self.path = path
         self.button = button
+        self.master = master
 
     def run(self):
         output = subprocess.getoutput('"C:\Program Files\Handbrake\HandBrakeCLI.exe" --input '
                                      + self.path + ' --title 0')
         #output = open("../handbrake_example.txt").read()
         data = handbrake.parse_handbrake(output)
-        root = tk.Tk()
-        app = RipperWindow.RipperWindow(self.path, data, master=root)
-        app.mainloop()
+        app = RipperWindow.RipperWindow(self.path, data, master=self.master)
+        #app.mainloop()
         self.button.config(state=tk.NORMAL)
 
 
